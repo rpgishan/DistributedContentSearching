9d242b310b9d901b1ca3072ff8533f10c7498e96
IrushiL <irushi@wso2.com>
2020-07-25 17:02:04 +0530

    Fix duplicate import

--- user-interface/src/Pages/FileSearchPage.jsx
+++ user-interface/src/Pages/FileSearchPage.jsx
@@ -14,7 +14,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import {Link} from 'react-router-dom';
-import {Link } from "react-router-dom";
 
 const styles = {
     container: {
